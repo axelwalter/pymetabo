2e2875fb215def45e841d0a3d79827e7e66dbb6c
axelwalter <axel.walter@uni-tuebingen.de>
2022-05-11 13:13:40 +0200

    fix getMetaValue

--- dataframes.py
+++ dataframes.py
@@ -7,11 +7,11 @@ class DataFrames:
         df = consensus_map.get_df().drop(["sequence"], axis=1)
         for cf in consensus_map:
             if cf.metaValueExists("best ion"):
-                df["adduct"] = [cf.getMetaValue("best ion", "") for cf in consensus_map]
+                df["adduct"] = [cf.getMetaValue("best ion") for cf in consensus_map]
                 break
         for cf in consensus_map:
             if cf.metaValueExists("best ion"):
-                df["name"] = [cf.getMetaValue("label", "") for cf in consensus_map]
+                df["name"] = [cf.getMetaValue("label") for cf in consensus_map]
                 break
         df.to_csv(table_file, sep="\t")
         return df
