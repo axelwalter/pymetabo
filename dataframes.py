74f719781dfb9fdc20d3055ade862c559011a447
axelwalter <axel.walter@uni-tuebingen.de>
2022-05-11 13:11:54 +0200

    export metavalues in ConsensusMap

--- dataframes.py
+++ dataframes.py
@@ -5,5 +5,13 @@ class DataFrames:
         consensus_map = ConsensusMap()
         ConsensusXMLFile().load(consensusXML_file, consensus_map)
         df = consensus_map.get_df().drop(["sequence"], axis=1)
+        for cf in consensus_map:
+            if cf.metaValueExists("best ion"):
+                df["adduct"] = [cf.getMetaValue("best ion", "") for cf in consensus_map]
+                break
+        for cf in consensus_map:
+            if cf.metaValueExists("best ion"):
+                df["name"] = [cf.getMetaValue("label", "") for cf in consensus_map]
+                break
         df.to_csv(table_file, sep="\t")
         return df
