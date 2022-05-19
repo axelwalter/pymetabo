ca2d23da0ea34bbaad0f097ee7bb8a371b9043e7
axelwalter <axel.walter@uni-tuebingen.de>
2022-05-19 14:59:28 +0200

    add sirius

--- example.py
+++ example.py
@@ -2,8 +2,9 @@ import os
 from pymetabo.core import *
 from pymetabo.helpers import *
 from pymetabo.dataframes import *
+from pymetabo.sirius import *
 
-mzML_dir = "/home/axel/Nextcloud/workspace/Tests/CentogeneWorkflowTest/mzML"
+mzML_dir = "/home/axel/Documents/validation_mzML"
 
 results = Helper().reset_directory("results")
 interim = Helper().reset_directory(os.path.join(results, "interim"))
@@ -29,27 +30,29 @@ PrecursorCorrector().to_nearest_feature(os.path.join(interim, "mzML_PCpeak"),
 MapAligner().run(os.path.join(interim, "mzML_PCfeature"), os.path.join(interim, "MzML_aligned"),
                 os.path.join(interim, "Trafo"))
 
-FeatureLinker().run(os.path.join(interim, "FFM_aligned"),
-                os.path.join(interim,  "FFM.consensusXML"))
+# FeatureLinker().run(os.path.join(interim, "FFM_aligned"),
+#                 os.path.join(interim,  "FFM.consensusXML"))
 
-DataFrames().create_consensus_table(os.path.join(
-    interim, "FFM.consensusXML"), os.path.join(interim, "FFM_consensus.tsv"))
+# DataFrames().create_consensus_table(os.path.join(
+#     interim, "FFM.consensusXML"), os.path.join(interim, "FFM_consensus.tsv"))
 
-FeatureMapHelper().FFMID_libraries_for_missing_features(os.path.join(interim,  "FFM.consensusXML"),
-                                                    os.path.join(interim,  "FFMID_libraries"))
+# FeatureMapHelper().FFMID_libraries_for_missing_features(os.path.join(interim,  "FFM.consensusXML"),
+#                                                     os.path.join(interim,  "FFMID_libraries"))
 
-FeatureFinderMetaboIdent().run(os.path.join(interim, "MzML_aligned"),
-                            os.path.join(interim,  "FFMID"),
-                            os.path.join(interim,  "FFMID_libraries"))
+# FeatureFinderMetaboIdent().run(os.path.join(interim, "MzML_aligned"),
+#                             os.path.join(interim,  "FFMID"),
+#                             os.path.join(interim,  "FFMID_libraries"))
 
-FeatureMapHelper().merge_feature_maps(os.path.join(interim, "FeatureMaps_merged"), os.path.join(
-    interim, "FFM"), os.path.join(interim, "FFMID"))
+# FeatureMapHelper().merge_feature_maps(os.path.join(interim, "FeatureMaps_merged"), os.path.join(
+#     interim, "FFM"), os.path.join(interim, "FFMID"))
 
-MetaboliteAdductDecharger().run(os.path.join(interim, "FeatureMaps_merged"), os.path.join(interim, "FeatureMaps_decharged"),
-                            {"potential_adducts": [b"H:+:0.5", b"Na:+:0.3", b"H-1O-1:+:0.2"],
-                            "charge_min": 1,
-                            "charge_max": 3,
-                            "max_neutrals": 2})
+MetaboliteAdductDecharger().run(os.path.join(interim, "FFM"), 
+                                os.path.join(interim, "FeatureMaps_decharged"),
+                                {"potential_adducts": [b"H:+:0.4",b"Na:+:0.2",b"NH4:+:0.2", 
+                                                        b"H-1O-1:+:0.1", b"H-3O-2:+:0.1"],
+                                "charge_min": 1,
+                                "charge_max": 3,
+                                "max_neutrals": 2})
 
 MapID().run(os.path.join(interim, "MzML_aligned"), os.path.join(
     interim, "FeatureMaps_decharged"), os.path.join(interim, "FeatureMaps_ID_mapped"))
@@ -59,3 +62,15 @@ FeatureLinker().run(os.path.join(interim, "FeatureMaps_ID_mapped"),
 
 DataFrames().create_consensus_table(os.path.join(
     interim, "FeatureMatrix.consensusXML"), os.path.join(results, "FeatureMatrix.tsv"))
+
+Sirius().run(os.path.join(interim, "MzML_aligned"), os.path.join(interim, "FeatureMaps_decharged"),
+            os.path.join(results, "Sirius"), params={
+                "preprocessing:filter_by_num_masstraces": 2,
+                "preprocessing:feature_only": "true",
+                "sirius:profile": "orbitrap",
+                "sirius:db": "none",
+                "sirius:ions_considered": "[M+H]+, [M-H2O+H]+, [M+Na]+, [M+NH4]+",
+                "sirius:elements_enforced": "CHN[15]OS[4]Cl[2]P[2]",
+                "project:processors": 2,
+                "fingerid:db": "BIO"
+            })
